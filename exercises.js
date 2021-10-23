8606164c0c0e541b633f3a21bd8fb1c021ab0fa1
Alex Waller <alex@artofalexwaller.com>
2021-10-23 16:49:07 -0400

    minMax()

---
+++ exercises.js
@@ -0,0 +1,42 @@
+// exercises
+let numbers1 = [1 , 3, 5, 7, 9, 11, 13, 15];
+
+let numbers2 = [2, 4, 6, 8, 10, 12, 14, 16];
+
+let numbers3 = [37];
+
+let testArray = [];
+
+function maxMin(numsArray = testArray){
+    if (numsArray.length == 0) console.log('Array is Empty.');
+    if (numsArray.length == 1) console.log('Array only has 1 number');
+    let max = numsArray[0];
+    let min = numsArray[0];
+    for (let i = 1; i < numsArray.length; i++){
+        let currentNum = numsArray[i];
+        if (currentNum > max){
+            max = currentNum;
+        }
+        if (currentNum < min){
+            min = currentNum;
+        }
+
+    }
+    console.log(`The max number is ${max} and the
+    min number is ${min}.`) ;
+    
+}
+
+maxMin();
+// Array is Empty.
+// The max number is undefined and the min number
+// is undefined
+
+maxMin(numbers1);
+// The max number is 15 and the min number is 1.
+maxMin(numbers2);
+// The max number is 16 and the min number is 2.
+
+maxMin(numbers3);
+// Array only has 1 number.
+// The max number is 37 and the min number is 37
---
+++ practice.js
@@ -0,0 +1,13 @@
+// practice
+console.log('Hi, Bodhi!!!');
+// Hi, Bodhi!!!
+let nums1 = [1, 3, 5, 2];
+
+if (nums1[0] == 1){
+    let max = 0;
+    for (let hh1 = 0; hh1 < nums1.length; hh1++){
+         max = Math.max(0, nums1[hh1]);
+    }
+    console.log(max);
+    // 2 so we don't want to use ,ath.max
+}
