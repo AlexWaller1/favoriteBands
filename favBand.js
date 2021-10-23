a79a322097dd03145439f3174be5116c129828cc
Alex Waller <alex@artofalexwaller.com>
2021-10-23 16:11:26 -0400

    initial commit

---
+++ favBand.js
@@ -0,0 +1,6 @@
+console.log('Hi, Bodhi!');
+// Hi, Bodhi!
+let favoriteBands = ['Smashing Pumpkins', 'White Stripes',
+'Red Hot Chili Bands', 'Hum', 'Rolling Stones'];
+console.log(favoriteBands);
+// ['Smashing Pumpkins', 'White Stripes', 'Red Hot Chili Bands', 'Hum', 'Rolling Stones']
