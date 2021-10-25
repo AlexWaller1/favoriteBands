f5d515fbd06bd77e0ba84b500a0570b6442285c0
Alex Waller <alex@artofalexwaller.com>
2021-10-24 22:17:52 -0400

    favoriteBands

--- favBand.js
+++ favBand.js
@@ -11,3 +11,51 @@ console.log(favoriteBands.includes('Journey'));
 let humInlet = favoriteBands.includes('Hum');
 console.log(humInlet);
 // true
+console.log('-------------------------------------------');
+console.log('-------------------------------------------');
+console.log('-------------------------------------------');
+
+const bandButton = document.querySelector('#favorite-band-button');
+const bandInput1 = document.querySelector('#favorite-band');
+const msg = document.querySelector('.msg');
+const bandList = document.querySelector('#favorite-bands-list');
+
+console.log(bandButton);
+
+console.log(bandInput1);
+
+console.log(msg);
+
+console.log(bandList);
+
+bandButton.addEventListener('click', onSubmit);
+
+function onSubmit(e){
+
+    if (bandInput1.value.trim() === '') {
+        msg.innerHTML = 'Please Fill Out Form.';
+    }
+    else if (favoriteBands.includes(bandInput1.value)){
+        msg.innerHTML = 'YEAH I LOVE THEM!';
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`
+        ${bandInput1.value}`));
+        bandList.appendChild(li);
+
+        // clear fields
+        bandInput1.value = '';
+    }
+    else {
+        msg.innerHTML = "Nah, they're pretty lame.";
+        const li1 = document.createElement('li');
+        li1.appendChild(document.createTextNode(`
+        ${bandInput1.value}`));
+        bandList.appendChild(li1);
+
+        // clear fields
+        bandInput1.value = '';
+    }
+
+}
+
+
