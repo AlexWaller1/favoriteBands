a1b77714de2143dfc60e9451984af65e38711afb
Alex Waller <alex@artofalexwaller.com>
2021-10-24 22:30:19 -0400

    toLowerCase()

--- favBand.js
+++ favBand.js
@@ -1,7 +1,10 @@
 console.log('Hi, Bodhi!');
 // Hi, Bodhi!
-let favoriteBands = ['Smashing Pumpkins', 'White Stripes',
-'Red Hot Chili Bands', 'Hum', 'Rolling Stones'];
+let favoriteBands = ['smashing pumpkins', 'white stripes',
+'red hot chili peppers', 'hum', 'rolling stones'];
+
+let userBands = [];
+
 console.log(favoriteBands);
 // ['Smashing Pumpkins', 'White Stripes', 'Red Hot Chili Bands', 'Hum', 'Rolling Stones']
 console.log(favoriteBands.includes('Smashing Pumpkins'));
@@ -32,15 +35,18 @@ bandButton.addEventListener('click', onSubmit);
 
 function onSubmit(e){
 
+
     if (bandInput1.value.trim() === '') {
         msg.innerHTML = 'Please Fill Out Form.';
     }
-    else if (favoriteBands.includes(bandInput1.value)){
+    else if (favoriteBands.includes(bandInput1.value.toLowerCase())){
         msg.innerHTML = 'YEAH I LOVE THEM!';
         const li = document.createElement('li');
         li.appendChild(document.createTextNode(`
         ${bandInput1.value}`));
         bandList.appendChild(li);
+        userBands.push(bandInput1.value);
+        console.log(bandInput1.value);
 
         // clear fields
         bandInput1.value = '';
@@ -51,6 +57,8 @@ function onSubmit(e){
         li1.appendChild(document.createTextNode(`
         ${bandInput1.value}`));
         bandList.appendChild(li1);
+        userBands.push(bandInput1.value);
+        console.log(userBands);
 
         // clear fields
         bandInput1.value = '';
